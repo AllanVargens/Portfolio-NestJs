bd4d21922879896feb2cd01b1574282b79154623
Clicou Mamou <allanvargens@gmail.com>
2024-02-29 17:25:57 -0300

    initial commit - project created

---
+++ back-nest-js/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb:mongodb://allanvargensDB:vargens@localhost:27017/',
+    ),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
