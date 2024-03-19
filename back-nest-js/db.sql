0cb264242eaefdc89b9d661aa8327559a1c00bdd
AllanVargens <allanvargens@gmail.com>
2024-03-18 21:01:17 -0300

    feat: models in schema.prisma created

--- back-nest-js/db.sql
+++ back-nest-js/db.sql
@@ -1,18 +1,7 @@
-Use "Portfolio";
-
 create table "users" (
-    "user_id" integer not null auto_increment primary key
-    "username" varchar(255) not null  
-    "password" varchar(255) not null
-    "email" varchar(255) not null
+    "user_id" varchar not null primary key, "username" text not null, "password" varchar(255) not null, "email" varchar(255) not null
 )
 
 create table "projects" (
-    "project_id" integer not null auto_increment primary key
-    "title" varchar(255) not null
-    "description" varchar(255) not null
-    "link" varchar(255) not null
-    "ImageURL" JSON
-    "user_id" integer not null
-    foreign key ("user_id") references "users" ("user_id")
+    "project_id" varchar not null primary key, "title" varchar(255) not null, "description" varchar(255) not null, "link" varchar(255) not null, "ImageURL" Array, "user_id" varchar not null foreign key, ("user_id") references "users" ("user_id")
 )
