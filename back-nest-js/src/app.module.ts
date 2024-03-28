880707aca3fc49e2bb628f0aceb963cc5b2210d1
AllanVargens <allanvargens@gmail.com>
2024-03-28 12:03:40 -0300

    projects resource created

--- back-nest-js/src/app.module.ts
+++ back-nest-js/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./users/users.module";
+import { ProjectsModule } from "./projects/projects.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule, ProjectsModule],
 })
 export class AppModule {}
