70874aa086d5dafdbed14ffdef231045a03c051a
AllanVargens <allanvargens@gmail.com>
2024-07-21 13:43:01 -0300

    feat: project bar terminated

--- front-end-angular17/src/app/components/card-project/card-project.component.ts
+++ front-end-angular17/src/app/components/card-project/card-project.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-project',
   standalone: true,
   imports: [],
   templateUrl: './card-project.component.html',
-  styleUrl: './card-project.component.scss'
+  styleUrl: './card-project.component.scss',
 })
 export class CardProjectComponent {
-
+  @Input() title: string = '';
+  @Input() image: string = '';
 }
--- front-end-angular17/src/app/components/project-bar/project-bar.component.ts
+++ front-end-angular17/src/app/components/project-bar/project-bar.component.ts
@@ -1,44 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CardProjectComponent } from '../card-project/card-project.component';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 type Project = {
   name: string;
-  image: string
-}
+  image: string;
+};
 
 @Component({
   selector: 'app-project-bar',
   standalone: true,
-  imports: [],
+  imports: [CardProjectComponent, CommonModule, NgOptimizedImage],
   templateUrl: './project-bar.component.html',
-  styleUrl: './project-bar.component.scss'
+  styleUrl: './project-bar.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ProjectBarComponent {
   projects: Project[] = [];
+  swiperEl: any;
 
-  ngOnInit(){
-    this.projects = [{
-      name: "Project 1",
-      image: "imagem-aleatoria.png"
-    },
-    {
-      name: "Project 2",
-      image: "imagem-aleatoria.png"
-    },
-    {
-      name: "Project 3",
-      image: "imagem-aleatoria.png"
-    },
-    {
-      name: "Project 4",
-      image: "imagem-aleatoria.png"
-    },
-    {
-      name: "Project 5",
-      image: "imagem-aleatoria.png"
-    },
-    {
-      name: "Project 6",
-      image: "imagem-aleatoria.png"
-    }]
+  ngOnInit() {
+    this.projects = [
+      {
+        name: 'Project 1',
+        image: 'imagem-aleatoria.png',
+      },
+      {
+        name: 'Project 2',
+        image: 'imagem-aleatoria.png',
+      },
+      {
+        name: 'Project 3',
+        image: 'imagem-aleatoria.png',
+      },
+      {
+        name: 'Project 4',
+        image: 'imagem-aleatoria.png',
+      },
+      {
+        name: 'Project 5',
+        image: 'imagem-aleatoria.png',
+      },
+      {
+        name: 'Project 6',
+        image: 'imagem-aleatoria.png',
+      },
+    ];
+    this.swiperEl = document.querySelector('swiper-container');
+    Object.assign(this.swiperEl, {
+      slidesPerView: 1,
+      spaceBetween: 5,
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 5,
+        },
+      },
+    });
+    this.swiperEl.initialize();
   }
 }
