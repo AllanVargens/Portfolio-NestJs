c4297d39ff3f62a2f2c646bddecf4284346f0c2c
AllanVargens <allanvargens@gmail.com>
2024-07-25 23:15:34 -0300

    (BACK) feat: Global AuthGuard Implemented

--- projects-api/src/app.module.ts
+++ projects-api/src/app.module.ts
@@ -5,10 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
   imports: [AuthModule, UsersModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    { provide: APP_GUARD, useClass: AuthGuard },
+  ],
 })
 export class AppModule {}
