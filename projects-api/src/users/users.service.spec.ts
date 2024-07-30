a3b795e6564ed0440a9051eecf953edb0a6bde74
AllanVargens <allanvargens@gmail.com>
2024-07-30 13:34:32 -0300

    (BACK) - user.service.spec.ts

--- projects-api/src/users/users.service.spec.ts
+++ projects-api/src/users/users.service.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { fakeUsers, prismaMock, prismaSelectQuery } from './users.mock';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: PrismaService, useValue: prismaMock },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return one user', async () => {
+      prisma.user.findUnique = jest.fn().mockResolvedValue(fakeUsers[0]);
+      const response = await service.findOne(fakeUsers[0].id);
+
+      expect(response).toBe(fakeUsers[0]);
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({
+        where: {
+          id: fakeUsers[0].id,
+        },
+        select: prismaSelectQuery,
+      });
+    });
   });
 });
