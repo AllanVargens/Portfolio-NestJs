74dfde3e2cd8eae7c95c81479455b418b1efc68e
AllanVargens <allanvargens@gmail.com>
2024-07-30 13:48:55 -0300

    (BACK) - feat : project entity file

--- projects-api/src/projects/entities/project.entity.ts
+++ projects-api/src/projects/entities/project.entity.ts
@@ -1,1 +1,15 @@
-export class Project {}
+import { Prisma } from '@prisma/client';
+
+export class Project {
+  id: number;
+  title: string;
+  description: string;
+  projectUrl: string;
+  userImage: string;
+  tagsBack: string[];
+  backendAbout: string;
+  tagsFront: string[];
+  frontendAbout: string;
+  sections: Prisma.SectionUncheckedCreateNestedManyWithoutProjectInput;
+  user: Prisma.UserCreateNestedOneWithoutProjectsInput;
+}
