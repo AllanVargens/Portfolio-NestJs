96b4c25121df1be7829d6ae9c110cfbc0ef28536
AllanVargens <allanvargens@gmail.com>
2024-08-03 23:32:02 -0300

    (BACK) feat: openAPI add to documentation

--- projects-api/src/main.ts
+++ projects-api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Projects API Documentation')
+    .setDescription('a api from register of projects')
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
