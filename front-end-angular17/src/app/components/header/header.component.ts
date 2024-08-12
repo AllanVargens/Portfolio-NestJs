1854ccb40571e86bdcab2efdfe4133d16535e44b
allan.vargens <allanvargens@gmail.com>
2024-08-12 10:40:40 -0300

    (FRONT) - feat: icons added in header

--- front-end-angular17/src/app/components/header/header.component.ts
+++ front-end-angular17/src/app/components/header/header.component.ts
@@ -1,15 +1,21 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import {  FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faPhone,  } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   isAnimating: boolean = false;
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faLinkedin = faLinkedin;
 
   animateH1() {
     const h1Element = document.querySelector("h1");
